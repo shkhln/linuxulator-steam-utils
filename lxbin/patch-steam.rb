92d1966d8359ed3f1e45e8397c48cfc69f159b1f
Alex S <shkhln@users.noreply.github.com>
2023-01-15 10:50:07 +0300

    We don't need to patch steamwebhelper.sh on every launch.

--- lxbin/patch-steam.rb
+++ lxbin/patch-steam.rb
@@ -3,7 +3,7 @@
 
 require 'fileutils'
 
-def patch_executable(path, out_path)
+def patch_executable(path, out_path, check_size = true)
 
   if not File.exist?(path)
     STDERR.puts "#{$PROGRAM_NAME}: #{path} not found, nothing to patch"
@@ -13,10 +13,14 @@ def patch_executable(path, out_path)
   def same_size(a, b); File.lstat(a).size == File.lstat(b).size; end
   def is_older (a, b); File.mtime(a)      <  File.mtime(b);      end
 
-  if not (File.exists?(out_path) && same_size(path, out_path) && is_older(path, out_path))
+  if not (File.exists?(out_path) && (check_size ? same_size(path, out_path) : true) && is_older(path, out_path))
     IO.binwrite(out_path, yield(IO.binread(path)))
     File.chmod(0700, out_path)
   end
+end
+
+def patch_script(path, out_path, &block)
+  patch_executable(path, out_path, false, &block)
 end
 
 steam_root = ENV['HOME'] + '/.steam/steam'
@@ -29,7 +33,7 @@ for f in ['ubuntu12_32/chromehtml.so', 'ubuntu12_32/steamclient.so', 'ubuntu12_3
   end
 end
 
-patch_executable(steam_root + '/ubuntu12_64/steamwebhelper.sh', steam_root + '/ubuntu12_64/steamwebhelper.sh.patched') do |obj|
+patch_script(steam_root + '/ubuntu12_64/steamwebhelper.sh', steam_root + '/ubuntu12_64/steamwebhelper.sh.patched') do |obj|
   obj.gsub!('"${in_container[@]}"', '')
   obj
 end
