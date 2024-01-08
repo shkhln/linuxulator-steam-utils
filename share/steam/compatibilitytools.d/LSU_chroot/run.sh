99a89655a0753550073a3a029b75a49b56cb3982
Alex S <shkhln@users.noreply.github.com>
2024-01-08 21:04:11 +0300

    Whatever.

---
+++ share/steam/compatibilitytools.d/LSU_chroot/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#TODO: sysctl -nq security.bsd.unprivileged_chroot
+export LD_LIBRARY_PATH="`printenv LD_LIBRARY_PATH | sed -e 's/steam-runtime/nope/g'`"
+export PATH="/bin"
+shift # waitforexitandrun
+exec env -u LD_PRELOAD /usr/local/../sbin/chroot -n $HOME/.steam/mnt /bin/env LD_PRELOAD="$LD_PRELOAD" "$@"
