01f56cc249abe2e761b5f6d7b54e4b3765d1488f
Alex S <shkhln@users.noreply.github.com>
2024-01-12 06:23:37 +0300

    Preserve cwd.

--- share/steam/compatibilitytools.d/LSU_chroot/run.sh
+++ share/steam/compatibilitytools.d/LSU_chroot/run.sh
@@ -3,4 +3,5 @@
 export LD_LIBRARY_PATH="`printenv LD_LIBRARY_PATH | sed -e 's/steam-runtime/nope/g'`"
 export PATH="/bin"
 shift # waitforexitandrun
-exec env -u LD_PRELOAD /usr/local/../sbin/chroot -n $HOME/.steam/mnt /bin/env LD_PRELOAD="$LD_PRELOAD" "$@"
+exec env -u LD_PRELOAD /usr/local/../sbin/chroot -n $HOME/.steam/mnt\
+ /bin/env LD_PRELOAD="$LD_PRELOAD" sh -c 'cd "$1" && shift && exec "$@"' - "$PWD" "$@"
