7bcf3ffa1bdef9fd72c6805e4a24a7605675b3b6
Alex S <shkhln@users.noreply.github.com>
2024-01-15 02:28:53 +0300

    Add a bit more elaborate chroot script.

--- share/steam/compatibilitytools.d/LSU_chroot/run.sh
+++ share/steam/compatibilitytools.d/LSU_chroot/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#TODO: sysctl -nq security.bsd.unprivileged_chroot
-export LD_LIBRARY_PATH="`printenv LD_LIBRARY_PATH | sed -e 's/steam-runtime/nope/g'`"
-export PATH="/bin"
-shift # waitforexitandrun
-exec env -u LD_PRELOAD /usr/local/../sbin/chroot -n $HOME/.steam/mnt\
- /bin/env LD_PRELOAD="$LD_PRELOAD" sh -c 'cd "$1" && shift && exec "$@"' - "$PWD" "$@"
+. lsu-linux-to-freebsd-env
+__dir__="$(dirname "$(realpath "$0")")"
+exec "$__dir__/run.rb" "$@"
