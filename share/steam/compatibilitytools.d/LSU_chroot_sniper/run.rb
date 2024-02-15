fc4ff37601cd4dc60cf51f79982599378166cfab
Alex S <shkhln@users.noreply.github.com>
2024-02-15 12:20:22 +0300

    Add bin/lsu-run-in-chroot, rename LSU_chroot into LSU_chroot_sniper.

---
+++ share/steam/compatibilitytools.d/LSU_chroot_sniper/run.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+SLR_DIR = 'SteamLinuxRuntime_sniper'
+
+def run(args)
+  ENV['LSU_LINUX_LD_PRELOAD']      = ['shmfix.so', ENV['LSU_LINUX_LD_PRELOAD']].compact.join(':')
+  ENV['LSU_LINUX_LD_LIBRARY_PATH'] = ENV['LSU_LINUX_LD_LIBRARY_PATH'].gsub(/[^:]+steam-runtime[^:]+:/, '')
+  ENV['LSU_LINUX_PATH']            = "/bin"
+
+  exec(File.expand_path('../../../../bin/lsu-run-in-chroot', __dir__), SLR_DIR, *args)
+end
+
+case ARGV[0]
+  when 'waitforexitandrun'
+    run(ARGV[1..-1])
+  else
+    puts "Unknown command #{ARGV[0]}"
+    exit(1)
+end
