2efccf70f1723d03d89510b3b994ecfb95832521
Alex S <shkhln@users.noreply.github.com>
2024-02-15 12:26:29 +0300

    Move compat tools dir.

--- tools/LSU_chroot_sniper/run.rb
+++ tools/LSU_chroot_sniper/run.rb
@@ -6,9 +6,9 @@ SLR_DIR = 'SteamLinuxRuntime_sniper'
 def run(args)
   ENV['LSU_LINUX_LD_PRELOAD']      = ['shmfix.so', ENV['LSU_LINUX_LD_PRELOAD']].compact.join(':')
   ENV['LSU_LINUX_LD_LIBRARY_PATH'] = ENV['LSU_LINUX_LD_LIBRARY_PATH'].gsub(/[^:]+steam-runtime[^:]+:/, '')
-  ENV['LSU_LINUX_PATH']            = "/bin"
+  ENV['LSU_LINUX_PATH']            = '/bin'
 
-  exec(File.expand_path('../../../../bin/lsu-run-in-chroot', __dir__), SLR_DIR, *args)
+  exec(File.expand_path('../../bin/lsu-run-in-chroot', __dir__), SLR_DIR, *args)
 end
 
 case ARGV[0]
