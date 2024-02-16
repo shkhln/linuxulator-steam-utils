d044d9b980f4404958e245c4d3223ac13a088927
Alex S <shkhln@users.noreply.github.com>
2024-02-16 14:21:59 +0300

    Whoops.

--- tools/LSU_chroot_sniper/run.rb
+++ tools/LSU_chroot_sniper/run.rb
@@ -6,6 +6,7 @@ SLR_DIR = 'SteamLinuxRuntime_sniper'
 case ARGV[0]
   when 'waitforexitandrun'
     ENV['LSU_LINUX_LD_LIBRARY_PATH'] = ENV['LSU_LINUX_LD_LIBRARY_PATH'].gsub(/[^:]+steam-runtime[^:]+:/, '')
+    ENV['LSU_LINUX_PATH']            = '/bin'
     exec(File.expand_path('../../bin/lsu-run-in-chroot', __dir__), SLR_DIR, *ARGV[1..-1])
   else
     puts "Unknown command #{ARGV[0]}"
