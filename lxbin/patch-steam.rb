faddf8d8e0d2e55d8f4e0dce6496f9e141b73be2
Alex S <shkhln@users.noreply.github.com>
2024-03-02 03:04:59 +0300

    Route steamwebhelper through chroot.

--- lxbin/patch-steam.rb
+++ lxbin/patch-steam.rb
@@ -13,7 +13,7 @@ def patch_executable(path, out_path, check_size = true)
   def same_size(a, b); File.lstat(a).size == File.lstat(b).size; end
   def is_older (a, b); File.mtime(a)      <  File.mtime(b);      end
 
-  if not (File.exists?(out_path) && (check_size ? same_size(path, out_path) : true) && is_older(path, out_path))
+  if !(File.exists?(out_path) && (check_size ? same_size(path, out_path) : true) && is_older(path, out_path) && is_older(__FILE__, out_path))
     IO.binwrite(out_path, yield(IO.binread(path)))
     File.chmod(0700, out_path)
   end
@@ -26,6 +26,7 @@ end
 steam_root = ENV['HOME'] + '/.steam/steam'
 
 patch_script(steam_root + '/ubuntu12_64/steamwebhelper.sh', steam_root + '/ubuntu12_64/steamwebhelper.sh.patched') do |obj|
-  obj.gsub!('"${in_container[@]}"', '')
+  obj.gsub!('mkdir -p "${STEAM_BASE_FOLDER}/logs"', 'mkdir -p "${STEAM_BASE_FOLDER}/logs" || true')
+  obj.gsub!('exec ${in_container[0]+"${in_container[@]}"}', 'exec lsu-webhelper-chroot')
   obj
 end
