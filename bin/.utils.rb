64448427e4a7898b787895c802a35d8323e70eee
Alex S <shkhln@users.noreply.github.com>
2024-03-11 11:15:14 +0300

    Clean up error handling a bit.

--- bin/.utils.rb
+++ bin/.utils.rb
@@ -88,6 +88,9 @@ def print_cmd(cmd)
   STDERR.puts cmd.map{|s| s =~ /\s/ ? s.inspect : s}.join(' ')
 end
 
+class MountError < ::StandardError
+end
+
 def mount(fs, from, to, options = nil)
   if fs == 'nullfs' && File.file?(from)
     FileUtils.touch(to)
@@ -102,12 +105,14 @@ def mount(fs, from, to, options = nil)
   cmd << from
   cmd << to
   print_cmd(cmd)
-  system(*cmd) || raise
+  system(*cmd) || raise(MountError.new("unable to mount #{from.inspect} to #{to.inspect}"))
   File.realpath(to)
 end
 
 def try_mount(fs, from, to, options = nil)
-  mount(fs, from, to, options) rescue nil
+  mount(fs, from, to, options)
+rescue MountError => ex
+  nil
 end
 
 def download_debs(dpkgs, dist_path)
@@ -134,34 +139,27 @@ def download_debs(dpkgs, dist_path)
 
   threads = []
   4.times do
-    threads <<
-      Thread.new do
-        begin
-          while e = queue.deq
-            pkg, meta  = e
-            downloaded = false
-            for mirror in mirrors
-              STDERR.puts "Downloading #{pkg} from #{mirror}..."
-              if system('fetch', '-o', File.join(dist_path, "#{pkg}.tmp"), "#{mirror}/#{meta[:path]}/#{pkg}")
-                downloaded = true
-                break
-              end
-            end
-            raise if !downloaded
-            FileUtils.mv(File.join(dist_path, "#{pkg}.tmp"), File.join(dist_path, pkg))
+    thread = Thread.new do
+      while e = queue.deq
+        pkg, meta  = e
+        downloaded = false
+        for mirror in mirrors
+          STDERR.puts "Downloading #{pkg} from #{mirror}..."
+          if system('fetch', '-o', File.join(dist_path, "#{pkg}.tmp"), "#{mirror}/#{meta[:path]}/#{pkg}")
+            downloaded = true
+            break
           end
-        rescue
-          next false
         end
-        true
-      end # thread
+        raise "unable to download #{pkg}" if !downloaded
+        FileUtils.mv(File.join(dist_path, "#{pkg}.tmp"), File.join(dist_path, pkg))
+      end
+    end # thread
+    thread.abort_on_exception  = false
+    thread.report_on_exception = false
+    threads << thread
   end
 
-  results = threads.map do |thread|
-    thread.join.value
-  end
-
-  raise if !results.reduce{|r, a| r && a}
+  threads.each(&:join)
 end
 
 def extract_debs(dpkgs, dist_path, target_path)
@@ -173,29 +171,26 @@ def extract_debs(dpkgs, dist_path, target_path)
 
   threads = []
   [`sysctl -nq hw.ncpu`.to_i, 16].min.times do
-    threads <<
-      Thread.new do
-        begin
-          while e = queue.deq
-            pkg, meta = e
-            sha256 = IO.popen(['sha256', '-q', File.join(dist_path, pkg)]){|io| io.read.chomp}
-            if sha256 != meta[:sha256]
-              STDERR.puts "Wrong checksum #{sha256} for #{pkg}, expected #{meta[:sha256]}."
-              raise
-            end
-            system('sh', '-c', 'tar --to-stdout -xf "$0" data.tar.zst | tar --cd "$1" -x',
-              '-', File.join(dist_path, pkg), target_path) || raise
-          end
-        rescue
-          next false
+    thread = Thread.new do
+      while e = queue.deq
+        pkg, meta = e
+        sha256 = IO.popen(['sha256', '-q', File.join(dist_path, pkg)]) do |io|
+          value = io.read.chomp
+          io.close
+          raise "unable to compute checksum of #{pkg}" if !$?.success?
+          value
         end
-        true
-      end # thread
+        if sha256 != meta[:sha256]
+          raise "wrong checksum #{sha256.inspect} for #{pkg}, expected #{meta[:sha256].inspect}"
+        end
+        system('sh', '-c', 'set -o pipefail && tar --to-stdout -xf "$0" data.tar.zst | tar --cd "$1" -x',
+          '-', File.join(dist_path, pkg), target_path) || raise("unable to extract #{pkg}")
+      end
+    end # thread
+    thread.abort_on_exception  = false
+    thread.report_on_exception = false
+    threads << thread
   end
 
-  results = threads.map do |thread|
-    thread.join.value
-  end
-
-  raise if !results.reduce{|r, a| r && a}
+  threads.each(&:join)
 end
