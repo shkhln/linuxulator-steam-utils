ae6c84515b53cc7dbc74de9a30f1b72388c21b09
Alex S <shkhln@users.noreply.github.com>
2024-03-13 08:48:05 +0300

    Set pins for the legacy runtime chroot.

--- tools/LSU_Scout_chroot/run.rb
+++ tools/LSU_Scout_chroot/run.rb
@@ -1,13 +1,28 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+require 'fileutils'
 require_relative '../../bin/.utils'
 
 SLR_DIR = 'SteamLinuxRuntime_sniper'
 
 case ARGV[0]
   when 'waitforexitandrun'
-    #TODO: what should we do with pins?
+    mroot = File.join(LSU_TMPDIR_PATH, File.basename(SLR_DIR))
+
+    FileUtils.mkdir_p(File.join(mroot, '/usr/steam-runtime/pinned_libs_64/'))
+    FileUtils.mkdir_p(File.join(mroot, '/usr/steam-runtime/pinned_libs_32/'))
+
+    # let's repeat scout-on-soldier's pins
+    system('ln', '-fhs', File.join(STEAM_ROOT_PATH, 'ubuntu12_32/steam-runtime/usr/lib/x86_64-linux-gnu/libcurl.so.4'),
+      File.join(mroot, '/usr/steam-runtime/pinned_libs_64/')) || raise
+    system('ln', '-fhs', File.join(STEAM_ROOT_PATH, 'ubuntu12_32/steam-runtime/usr/lib/x86_64-linux-gnu/libcurl.so.3'),
+      File.join(mroot, '/usr/steam-runtime/pinned_libs_64/')) || raise
+    system('ln', '-fhs', File.join(STEAM_ROOT_PATH, 'ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu/libcurl.so.4'),
+      File.join(mroot, '/usr/steam-runtime/pinned_libs_32/')) || raise
+    system('ln', '-fhs', File.join(STEAM_ROOT_PATH, 'ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu/libcurl.so.3'),
+      File.join(mroot, '/usr/steam-runtime/pinned_libs_32/')) || raise
+
     steam_runtime_libs = ENV['LSU_LINUX_LD_LIBRARY_PATH'].split(':')
       .find_all{|path| path =~ /\/steam-runtime\// && !(path =~ /\/pinned_libs_(32|64)/)}
 
@@ -20,6 +35,8 @@ case ARGV[0]
       File.join(LSU_IN_CHROOT, 'lib64/noepollexcl'),
       File.join(LSU_IN_CHROOT, 'lib32/shmfix'),
       File.join(LSU_IN_CHROOT, 'lib64/shmfix'),
+      '/usr/steam-runtime/pinned_libs_32',
+      '/usr/steam-runtime/pinned_libs_64',
       '/lib/i386-linux-gnu',
       '/lib/x86_64-linux-gnu',
       '/usr/lib/i386-linux-gnu',
